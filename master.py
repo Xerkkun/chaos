0d4655cd60c250e212615bff23141e53c05cd32d
Xerkkun <xerk.kun@gmail.com>
2021-05-21 14:52:23 -0500

    RK4 method ready

--- master.py
+++ master.py
@@ -29,6 +29,38 @@ def Fx4(x,y,z,w):
 def forward_euler(xn,yn,zn,wn,h):
     x,y,z,w = xn+h*Fx1(xn,yn,zn,wn),yn+h*Fx2(xn,yn,zn,wn),zn+h*Fx3(xn,yn,zn,wn),wn+h*Fx4(xn,yn,zn,wn)
     return x,y,z,w
+#-------------------------------------------------------------------------------
+def backward_euler(xn,yn,zn,wn,h):
+    xn1,yn1,zn1,wn1 = forward_euler(xn,yn,zn,wn,h)
+    x,y,z,w = xn+h*Fx1(xn1,yn1,zn1,wn1),yn+h*Fx2(xn1,yn1,zn1,wn1),zn+h*Fx3(xn1,yn1,zn1,wn1),wn+h*Fx4(xn1,yn1,zn1,wn1)
+    return x,y,z,w
+#-------------------------------------------------------------------------------
+def runge_kutta4(xn,yn,zn,wn,h):
+    k1 = h*Fx1(xn,yn,zn,wn)
+    k2 = Fx1(xn + h*0.5*k1,yn + h*0.5*k1,zn + h*0.5*k1,wn + h*0.5*k1)
+    k3 = Fx1(xn + h*0.5*k2,yn + h*0.5*k2,zn + h*0.5*k2,wn + h*0.5*k2)
+    k4 = Fx1(xn + h*k3,yn + h*k3,zn + h*k3,wn + h*k3)
+    x = xn + (h/6.)*(k1 + 2*k2 + 2*k3 + k4)
+
+    k1 = h*Fx2(xn,yn,zn,wn)
+    k2 = Fx2(xn + h*0.5*k1,yn + h*0.5*k1,zn + h*0.5*k1,wn + h*0.5*k1)
+    k3 = Fx2(xn + h*0.5*k2,yn + h*0.5*k2,zn + h*0.5*k2,wn + h*0.5*k2)
+    k4 = Fx2(xn + h*k3,yn + h*k3,zn + h*k3,wn + h*k3)
+    y = yn + (h/6.)*(k1 + 2*k2 + 2*k3 + k4)
+
+    k1 = h*Fx3(xn,yn,zn,wn)
+    k2 = Fx3(xn + h*0.5*k1,yn + h*0.5*k1,zn + h*0.5*k1,wn + h*0.5*k1)
+    k3 = Fx3(xn + h*0.5*k2,yn + h*0.5*k2,zn + h*0.5*k2,wn + h*0.5*k2)
+    k4 = Fx3(xn + h*k3,yn + h*k3,zn + h*k3,wn + h*k3)
+    z = zn + (h/6.)*(k1 + 2*k2 + 2*k3 + k4)
+
+    k1 = h*Fx4(xn,yn,zn,wn)
+    k2 = Fx4(xn + h*0.5*k1,yn + h*0.5*k1,zn + h*0.5*k1,wn + h*0.5*k1)
+    k3 = Fx4(xn + h*0.5*k2,yn + h*0.5*k2,zn + h*0.5*k2,wn + h*0.5*k2)
+    k4 = Fx4(xn + h*k3,yn + h*k3,zn + h*k3,wn + h*k3)
+    w = wn + (h/6.)*(k1 + 2*k2 + 2*k3 + k4)
+
+    return x,y,z,w
 #===============================================================================
 #Métodos de secuencias binarias
 def umbral(x,y,z,w,sel):
@@ -103,6 +135,12 @@ while r < s:
     if met == 'FE':
         h = hh[0]
         x,y,z,w = forward_euler(xn,yn,zn,wn,h)
+    elif met == 'BE':
+        h = hh[1]
+        x,y,z,w = backward_euler(xn,yn,zn,wn,h)
+    elif met == 'RK4':
+        h = hh[2]
+        x,y,z,w = runge_kutta4(xn,yn,zn,wn,h)
 
     if abs(x)>50:
         print("Overflow in r = ",r)
